01823d4976704644a89e5a519366a3344055916b
juancmb27 <juanmontealegreb.27@gmail.com>
2023-07-13 16:35:41 -0500

    cuarto comentario

--- Calc.js
+++ Calc.js
@@ -1,5 +1,14 @@
 //dark mode//
-
+function enabledarkmode(){
+    let main_body = document.body;
+    main_body.classList.toggle("dark-mode");
+    
+    if (document.getElementById('checkitem').checked){
+        console.log('checked');
+    } else {
+        console.log("NOT checked");
+    }
+}
 
 const botonNumeros = document.getElementsByName("data-number");
 
